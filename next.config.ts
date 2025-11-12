3b18297a7d1dc5f9e6f61fd3267c7c035e758377
Steeven1995 <gabinmoundziegou@gmail.com>
2025-11-12 13:04:50 +0100

    add the allow origin all

--- next.config.ts
+++ next.config.ts
@@ -16,12 +16,10 @@ const nextConfig: NextConfig = {
         // Appliquer ces headers à toutes les routes API
         source: "/api/:path*",
         headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: process.env.NODE_ENV === "production" 
-              ? "https://livreur.inotech-gabon.com" 
-              : "*",
-          },
+           {
+          key: "Access-Control-Allow-Origin",
+          value: "*",
+        },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
@@ -36,7 +34,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Access-Control-Allow-Credentials",
-            value: "false",
+            value: "true",
           },
         ],
       },
